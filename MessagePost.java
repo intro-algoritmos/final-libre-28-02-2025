9fc4a5bf3fb02ee26ad77f17ca28e0106cc0371c
nmaguirre <naguirre@dc.exa.unrc.edu.ar>
2025-02-28 08:16:54 -0300

    initial import

---
+++ MessagePost.java
@@ -0,0 +1,107 @@
+import java.util.ArrayList;
+
+/**
+ * Esta clase captura información sobre un post en una red social. 
+ * El post es un mensaje en formato texto, que consta del username 
+ * (quien postea el mensaje), el mensaje en sí mismo, un timestamp y 
+ * una cantidad de likes. 
+ * 
+ * @author Michael Kölling and David J. Barnes
+ * @version 0.1
+ */
+public class MessagePost 
+{
+    // usuario que postea el message post
+    private String username;  
+    
+    // contenido del mensaje
+    private String message;   
+    
+    // número que indica el timestamp o momento de tiempo de creación del mensaje
+    private long timestamp;
+    
+    // cantidad de likes
+    private int likes;
+
+    /**
+     * Constructor de objetos de la clase MessagePost. La creación setea tanto el username como el 
+     * contenido del mensaje con los parámetros respectivos del método.
+     * El timestamp se setea con la hora actual en milisegundos.
+     * La cantidad de likes es cero.
+     * El autor no puede ser nulo ni vacío.
+     * El mensaje no puede ser nulo ni vacío.
+     * 
+     * @param author    el usuario autor del mensaje
+     * @param text      el texto del mensaje
+     */
+    public MessagePost(String author, String text)
+    {
+        //TODO implementar este método incluyendo chequeo de precondición
+    }
+
+    /**
+     * Agrega un "like" al mensaje actual
+     */
+    public void like()
+    {
+        likes++;
+    }
+
+    /**
+     * Retira un "like" del mensaje actual
+     */
+    public void unlike()
+    {
+        if (likes > 0) {
+            likes--;
+        }
+    }
+
+
+    /**
+     * Retorna el texto del post
+     * 
+     * @return el texto del post
+     */
+    public String getText()
+    {
+        return message;
+    }
+
+    /**
+     * Retorna el tiempo de creación de este post
+     * 
+     * @return el tiempo de creación del post.
+     */
+    public long getTimeStamp()
+    {
+        return timestamp;
+    }
+
+
+    /**
+     * Invariante de la clase MessagePost.
+     * Un messagepost es internamente consistente si su username es no nulo y no vacío, 
+     * su mensaje es no nulo y no vacío, el timestamp es un valor positivo, y la cantidad
+     * de likes es mayor o igual a cero.
+     * @return true ssi el objeto es internamente consistente (satisface el invariante de clase).
+     */
+    public boolean repOK() 
+    {
+        //TODO Implementar este método
+        return false;    
+    }
+    
+    
+    /**
+     * Retorna los detalles del message post, como
+     * cadena de caracteres.
+     * @return detalles del messagepost en una cadena de caracteres,
+     * usando el formato "<username>: <message> (posteado en <timestamp>). A <likes> personas le gustó este mensaje.".
+     */
+    public String toString()
+    {
+        //TODO Implementar este método
+        return null;
+    }
+}
---
+++ NewsFeed.java
@@ -0,0 +1,87 @@
+import java.util.ArrayList;
+
+/**
+ * La clase NewsFeed almacena posts de noticias de una red social.
+ * 
+ * El estado del newsfeed consta de una lista de todos los posts de mensajes 
+ * (objetos de tipo MessagePost). La lista está ordenada por timestamp. 
+ * 
+ * 
+ * @author Michael Kölling and David J. Barnes
+ * @version 0.1
+ */
+public class NewsFeed
+{
+    
+    // posts del newsfeed
+    private ArrayList<MessagePost> messages;
+
+    /**
+     * Construye un newsfeed vacío
+     */
+    public NewsFeed()
+    {
+        messages = new ArrayList<>();
+    }
+
+    /**
+     * Añade un mensaje de texto al newsfeed. El mensaje a agregar
+     * se ubica en la lista de mensajes de manera ordenada por timestamp. El 
+     * mensaje no puede ser nulo, debe satisfacer su respectivo invariante,
+     * y no debe existir en la lista otro mensaje con el mismo timestamp.
+     * 
+     * @param text es el mensaje de texto a agregar.
+     */
+    public void addMessagePost(MessagePost message)
+    {
+        //TODO implementar este método, incluyendo chequeo de precondición.
+    }
+
+
+    /**
+     * Elimina de la lista de mensajes todos los posts de un username determinado. 
+     * @param username es el username cuyos posts se quiere eliminar
+     * Precondición: el parámetro no debe ser nulo ni vacío.
+     * Postcondición: Se eliminar de la lista de posts todos los posts cuyo autor (username)
+     * coincida con el parámetro del método. 
+     */
+    public void eliminarUsername(String username)
+    {
+        //TODO: Implementar este método, incluyendo chequeo de precondición.
+    }
+    
+    /**
+     * Retorna la lista de posts en las cuales el número de likes es cero.
+     * @return la lista de mensajes (texto del mensaje) que poseen cero likes.
+     */
+    public ArrayList<String> ceroLikes()
+    {
+        return null;
+        //TODO Implementar este método, incluyendo posiblemente chequeo de precondición
+    }
+    
+    /**
+     * Retorna el message post cuyo timestamp es el más cercano posible al parámetro del método
+     * @param time es el tiempo (timestamp) con el cual se deben comparar los timestamps de la lista de posts
+     * @return el post más cercano al valor time pasado como parámetro.
+     */
+    public MessagePost masCercana(int time)
+    {
+        //TODO Implementar este método, incluyendo posiblemente chequeo de precondición
+        return null;
+    }
+    
+    /**
+     * Invariante de clase de NewsFeed. Un NewsFeed se considera válido, o
+     * internamente consistente, si su lista de posts no es nula, todos los posts de la lista
+     * son no nulos, satisfacen su respectivo repOK, no hay timestamps repetidos, y los posts
+     * se encuentran en la lista ordenados por timestamp.
+     * @return true si y sólo si el objeto satisface el invariante de clase.
+     */
+    public boolean repOK()
+    {
+        //TODO Implementar este método
+        return false;
+    }
+    
+}
